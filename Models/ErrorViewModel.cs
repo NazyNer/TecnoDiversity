353232b0595ec516ef2ac41bd0a28f8291c66827
Arcangel Maldonado <nemaldonado663@gmail.com>
2024-02-25 21:28:06 -0300

    V0.0.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TecnoDiversity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
